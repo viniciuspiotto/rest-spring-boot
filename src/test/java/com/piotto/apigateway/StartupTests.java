631a26d8862fe1b9f34845d4163de16b42371220
viniciuspiotto <viniciuspiotto.dev@gmail.com>
2024-07-23 13:09:43 -0300

    feat: initial commit with base project structure

---
+++ src/test/java/com/piotto/apigateway/StartupTests.java
@@ -0,0 +1,13 @@
+package com.piotto.apigateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StartupTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}

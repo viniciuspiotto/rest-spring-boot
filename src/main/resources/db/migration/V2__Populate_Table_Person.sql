bb8d8ab43e7b1a1f1dca6381d243af4e690ea860
viniciuspiotto <viniciuspiotto.dev@gmail.com>
2024-07-29 16:00:23 -0300

    feat(db): Implement migration in the application
    Implemented Flyway for database migrations. This is a best practice to manage database schema changes and helps restore the database in case of issues. Created initial migrations to set up the 'person' table and populate it.

---
+++ src/main/resources/db/migration/V2__Populate_Table_Person.sql
@@ -0,0 +1,6 @@
+LOCK TABLES `person` WRITE;
+INSERT INTO `person` VALUES (1,'Afonso Guimaraes','Vinicius Henrique Piotto','Male','Boiago'),
+                            (2,'Afonso Guimaraes','Cassio Felipe Piotto','Male','Boiago'),
+                            (3,'Afonso Guimaraes','Carlos Cesar','Male','Boiago'),
+                            (4,'Afonso Guimaraes','Alba Valeria','Female','Piotto');
+UNLOCK TABLES;
